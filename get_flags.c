37bbf3936d3196ce23a6ab4925bd7326a6339fcb
nkirue <133280824+nkirue@users.noreply.github.com>
2023-07-25 19:46:27 -0700

    Updated

--- get_flags.c
+++ get_flags.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
+ * get_flags - This calculates active flags
+ * @format: The formatted string in which to print the arguments
+ * @i: Take a parameter.
  * Return: Flags:
  */
 int get_flags(const char *format, int *i)
