fb81cb1ed61442e90f6a814f68f22a565351999e
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2023-07-25 19:34:01 -0700

    Updated

--- main.h
+++ main.h
@@ -57,15 +57,15 @@ int print_string(va_list types, char buffer[],
 int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[],
-        int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_octal(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
-        int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 int print_unsigned(va_list types, char buffer[],
-        int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
